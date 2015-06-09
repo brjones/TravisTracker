ce00f41d35f4934d5aea83e7a8d4c990abaafe97
James Glover <james.glover@sanger.ac.uk>
2015-06-09 09:24:01 +0100

    Switch to modular design

--- app.rb
+++ app.rb
@@ -1,19 +1,22 @@
 require 'rubygems'
-require 'sinatra'
-require 'travis'
+require 'sinatra/base'
 require 'sinatra/config_file'
+require 'tilt/erb'
 
+class TravisTracker < Sinatra::Base
+  register Sinatra::ConfigFile
+  require 'travis'
 
-config_file "config.yml"
+  config_file "config.yml"
 
-
-get '/' do
-  @states = {"failed" => "danger", "passed" => "success", "errored" => "warning", "started" => "info", "created" => "info"}
-  repos = []
-  settings.projects.each do |repo_name|
-    repos <<  Travis::Repository.find(repo_name)
+  get '/' do
+    @states = {"failed" => "danger", "passed" => "success", "errored" => "warning", "started" => "info", "created" => "info"}
+    repos = []
+    settings.projects.each do |repo_name|
+      repos <<  Travis::Repository.find(repo_name)
+    end
+    @repos = repos.each_slice(3).to_a
+    erb :'index'
   end
-  @repos = repos.each_slice(3).to_a
-  erb :'index'
-end
 
+end
