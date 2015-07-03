f37836b970875213c56ee1912db0260b12e66166
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-07-03 16:52:46 +0100

    Fixes to first testing

--- models/service_presenter.rb
+++ models/service_presenter.rb
@@ -8,8 +8,9 @@ class ServicePresenter
   attr_reader :environment, :application, :process
   def initialize(service)
     @service = service
-    name_array = service.name.split('.')
+    name_array = service.name.split('_').map{|s| s.split('.')}.flatten
     name_array << UNSPECIFIED_PROCESS if name_array.length < 3
+    #binding.pry
     name_array.unshift(UNSPECIIFED_ENVIRONMENT) if name_array.length < 3
     @environment = name_array.shift.humanize
     @application = name_array.shift.humanize
@@ -35,12 +36,24 @@ class ServicePresenter
   end
 
   def protocol
+    return '' if @service.port.nil?
+    unless @service.port.length.nil?
+      return @service.port[0]['protocol']
+    end
     @service.port['protocol']
   end
   def portnumber
+    return '' if @service.port.nil?
+    unless @service.port.length.nil?
+      return @service.port[0]['portnumber']
+    end
     @service.port['portnumber']
   end
   def hostname
+    return '' if @service.port.nil?
+    unless @service.port.length.nil?
+      return @service.port[0]['hostname']
+    end
     @service.port['hostname']
   end
 
